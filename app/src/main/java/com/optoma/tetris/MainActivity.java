43860d1f1244c9da2e08d6fd29a6be1737e28908
david lin <woei66@gmail.com>
2017-04-04 22:48:34 +0800

    fix

--- app/src/main/java/com/optoma/tetris/MainActivity.java
+++ app/src/main/java/com/optoma/tetris/MainActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import org.opencv.android.OpenCVLoader;
+import android.util.Log;
 /*
   how to use import opencv sdk to android studio, please visit
   http://stackoverflow.com/questions/17767557/how-to-use-opencv-in-android-studio-using-gradle-build-tool/27356635#27356635
@@ -20,6 +21,12 @@ public class MainActivity extends Activity {
 
         findViewById(R.id.ActivityMain_NewGameBtn).setOnClickListener(onNewGameBtnClick);
         findViewById(R.id.ActivityMain_ExitBtn).setOnClickListener(onExitBtnClick);
+
+        /*if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }*/
     }
 
     private View.OnClickListener onNewGameBtnClick = new View.OnClickListener() {
